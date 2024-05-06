99599c212f609ea2ef687e2fad454814ac6977f1
Unique <mujeem000@gmail.com>
2024-05-06 08:55:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    API_ID = environ.get("API_ID", "21233086")
+    API_HASH = environ.get("API_HASH", "2892db755e485aaf8e55b4b70e8953f6")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6749483929:AAEh5WwJFEUDe9DXQ-PsibVdZkavDzZtP0o") 
+    BOT_SESSION = environ.get("BOT_SESSION", "BQFD_b4AbWZBI8YFXZw_NsoqFtpReuRGAjb7boCz76O3entC9nVnrRSg2RwoDTys_wuJsJWLtQdMqp3WTFZ8m6LS6LFGTwvIm4Czclvt8yUBE9NIMQHqtAWJXSYPHZF893NNOQPl2y1sxfp6nFWLUb87sUzPn9kaBekZIbDFcYDPIKtUvJXM1a9PU5qV5qgSIE_OwXzgum9sl3IuPj62-F1dZGo0SLUHH1cxx1V--dG3FojJRsROhDJ4lzz1QOxQXShxuPduWwezf3Rb_rOmk0RVXWRgOxqOGGBZBVUhVkWEX2aewIdmaxBHnBbLNW9nuiQ8y6R4K7pAQlKqx3bL_3DbncEYzAAAAAGnqYHnAA") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
